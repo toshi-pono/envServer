428292cc4b8049e3ea7b7034a5f1f74a2ec38905
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-05-30 00:44:36 +0900

    データベースの接続を設定

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module env-server
 
 go 1.15
+
+require (
+	github.com/jmoiron/sqlx v1.3.4
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/lib/pq v1.10.2
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,74 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo"
+	_ "github.com/lib/pq"
+)
+
+type EnvData struct {
+	Id          int       `json:"id,omitempty" db:"id"`
+	Temperature float32   `json:"temperature" db:"temperature"`
+	Humidity    float32   `json:"humidity" db:"humidity"`
+	Pressure    float32   `json:"pressure" db:"pressure"`
+	Battery     float32   `json:"battery" db:"battery"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+}
+
+// テンプレートのレンダラ―を作る
+type Renderer struct {
+	templates *template.Template
+}
+
+func (r *Renderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return r.templates.ExecuteTemplate(w, name, data)
+}
+
+var (
+	db *sqlx.DB
 )
 
 func main() {
-	// トップページ
-	http.Handle("/", http.FileServer(http.Dir("static")))
+	_db, err := sqlx.Connect("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = _db
+
+	e := echo.New()
+
+	e.Renderer = &Renderer{
+		templates: template.Must(template.ParseGlob("static/*.html")),
+	}
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", nil)
+	})
+	e.POST("/api/data/postDatas", HandleAPIPostDatas)
+
 	// プロセスを起動
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func HandleAPIPostDatas(c echo.Context) error {
+	req := new(EnvData)
+	if err := c.Bind(req); err != nil {
+		return err
+	}
+	// データベースに追加
+	_, err := db.Exec("INSERT INTO weather (temperature, humidity, pressure, battery, created_at) VALUES (?, ?, ?, ?, ?)", req.Temperature, req.Humidity, req.Pressure, req.Battery, req.CreatedAt)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("db error: %v", err))
+	}
+	return c.String(http.StatusOK, "OK")
 }
