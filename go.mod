d2207b5894edb74b5ee6dfaa7860feeb3227319b
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-05-30 14:08:37 +0900

    Add LinebotBasicSystem

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/lib/pq v1.10.2
+	github.com/line/line-bot-sdk-go/v7 v7.9.1
 	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 )
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"time"
@@ -13,6 +14,8 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo"
 	_ "github.com/lib/pq"
+
+	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
 type PostData struct {
@@ -38,32 +41,111 @@ func (r *Renderer) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return r.templates.ExecuteTemplate(w, name, data)
 }
 
-var (
-	db *sqlx.DB
+const (
+	verifyToken = "00000000000000000000000000000000"
 )
 
-func main() {
-	fmt.Println("hello")
+var (
+	db  *sqlx.DB
+	bot *linebot.Client
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
 	_db, err := sqlx.Connect("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	db = _db
 
+	// LINEのAPIを利用する設定
+	_bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_ACCESS_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bot = _bot
+}
+
+func main() {
 	e := echo.New()
 
 	e.Renderer = &Renderer{
 		templates: template.Must(template.ParseGlob("static/*.html")),
 	}
 
+	// LINE LIFE
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "index", nil)
 	})
+
+	// DATA
 	e.GET("/data/latest", getLatestDataHandler)
 	e.POST("/data/postData", postDataHandler)
 
+	// LINEbot
+	e.POST("/callback", linebotHandler)
+
 	// プロセスを起動
 	e.Start(":" + os.Getenv("PORT"))
+}
+
+// LINEbotのリプライメッセージを扱う
+func linebotHandler(c echo.Context) error {
+	log.Println("Accessed")
+
+	events, err := bot.ParseRequest(c.Request())
+	switch err {
+	case nil:
+	case linebot.ErrInvalidSignature:
+		log.Println("PaeseRequest error:", err)
+		return c.NoContent(http.StatusBadRequest)
+	default:
+		log.Println("PaeseRequest error:", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	// メッセージの種類によって処理を変更
+	for _, event := range events {
+		// LINEサーバーからのverify時
+		if event.ReplyToken == verifyToken {
+			return c.NoContent(http.StatusOK)
+		}
+
+		switch event.Type {
+		// メッセージ受信時
+		case linebot.EventTypeMessage:
+			replyMessage := getReplyMessage(event)
+			// 返信を送信
+			if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+				log.Print(err)
+			}
+		// それ以外
+		default:
+			continue
+		}
+	}
+	return c.NoContent(http.StatusOK)
+}
+
+const helpMessage = `使い方
+使い方の説明を書きます！！！（未完）
+`
+
+func getReplyMessage(event *linebot.Event) string {
+	switch message := event.Message.(type) {
+	// テキストメッセージ
+	case *linebot.TextMessage:
+		return message.Text
+	// スタンプ
+	case *linebot.StickerMessage:
+		return fmt.Sprintf("sticker id is %v, stickerResourceType is %v", message.StickerID, message.StickerResourceType)
+	// それ以外
+	default:
+		return helpMessage
+	}
 }
 
 func getLatestDataHandler(c echo.Context) error {
