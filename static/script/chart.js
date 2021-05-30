eea466578f2eb85e6c97e94fcfa0347d95cbc1da
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-05-31 03:05:42 +0900

    Update 10分ごとの平均を出力する形式に変更

--- static/script/chart.js
+++ static/script/chart.js
@@ -6,31 +6,59 @@ window.onload = () => {
 const drawChart = async () => {
   const response = await fetch("/data/oneday");
   const datas = await response.json();
+
   let columns = [];
   let columns2 = [];
   let timeStamp = ["x"];
   let temperature = ["temperature"];
   let humidity = ["humidity"];
   let pressure = ["pressure"];
+
+  let cnt = 0;
+  let before = datas[0];
+  let humiditySum = 0;
+  let temperatureSum = 0;
+  let pressureSum = 0;
+  let minutesSum = 0;
+  // 10分間の平均を出力
   for (let i = 0; i < datas.length; i++) {
-    temperature.push(datas[i].temperature);
-    humidity.push(datas[i].humidity);
-    pressure.push(datas[i].pressure);
+    temperatureSum += datas[i].temperature;
+    humiditySum += datas[i].humidity;
+    pressureSum += datas[i].pressure;
+    minutesSum += new Date(datas[i].created_at).getUTCMinutes();
+    cnt++;
+    if (
+      !isTimeNear(datas[i].created_at, before.created_at) ||
+      datas.length < 10
+    ) {
+      // 平均を追加
+      temperature.push(temperatureSum / cnt);
+      humidity.push(humiditySum / cnt);
+      pressure.push(pressureSum / cnt);
 
-    const d = new Date(datas[i].created_at);
-    // HACK:データベースにJSTをUTCとして入れてしまっている
-    const formattedTime = `${d.getUTCFullYear()}-${d
-      .getUTCMonth()
-      .toString()
-      .padStart(2, "0")}-${d.getUTCDate().toString().padStart(2, "0")} ${d
-      .getUTCHours()
-      .toString()
-      .padStart(2, "0")}:${d.getUTCMinutes().toString().padStart(2, "0")}:${d
-      .getUTCSeconds()
-      .toString()
-      .padStart(2, "0")}`;
+      // 時刻の処理
+      const d = new Date(datas[i].created_at);
+      // HACK:データベースにJSTをUTCとして入れてしまっている
+      const formattedTime = `${d.getUTCFullYear()}-${d
+        .getUTCMonth()
+        .toString()
+        .padStart(2, "0")}-${d.getUTCDate().toString().padStart(2, "0")} ${d
+        .getUTCHours()
+        .toString()
+        .padStart(2, "0")}:${Math.round(minutesSum / cnt)
+        .toString()
+        .padStart(2, "0")}:${d.getUTCSeconds().toString().padStart(2, "0")}`;
+      timeStamp.push(formattedTime);
 
-    timeStamp.push(formattedTime);
+      // 変数初期化
+      humiditySum = 0;
+      temperatureSum = 0;
+      pressureSum = 0;
+      minutesSum = 0;
+      cnt = 0;
+    }
+
+    before = datas[i];
   }
   columns.push(timeStamp, temperature, humidity);
   columns2.push(timeStamp, pressure);
@@ -82,4 +110,18 @@ const drawChart = async () => {
       show: false,
     },
   });
+};
+
+// 分の10の位まで同じかどうか判定する
+const isTimeNear = (nowDate, beforeDate) => {
+  let nowD = new Date(nowDate);
+  let beforeD = new Date(beforeDate);
+  return (
+    nowD.getUTCFullYear() == beforeD.getUTCFullYear() &&
+    nowD.getUTCMonth() == beforeD.getUTCMonth() &&
+    nowD.getUTCDate() == beforeD.getUTCDate() &&
+    nowD.getUTCHours() == beforeD.getUTCHours() &&
+    nowD.getUTCMinutes().toString().padStart(2, "0")[0] ==
+      beforeD.getUTCMinutes().toString().padStart(2, "0")[0]
+  );
 };
